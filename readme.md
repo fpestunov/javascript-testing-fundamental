d78f5645f1e26efc06e124b3110ddfbda57050be
fpestunov <fpestunov@gmail.com>
2019-02-23 11:04:15 +0400

    add stubs test example

--- readme.md
+++ readme.md
@@ -31,3 +31,28 @@ Mocks are a common tool when writing tests for your software. In this lesson, we
 If we talk of a Mock, we mean an alternate implementation of a Dependency, when we are testing our code.
 
 Here is mock example `mocks.js`.
+
+## What are stubs?
+We're learning what stubs are when it comes to JavaScript testing.
+
+Stubs are similar to mocks, but they only replace small parts of your code compared to Mocks who replace the entire dependency.
+
+To make our example easier we'll just evoke function itself, so, we can see the outcome
+on the command line, as we did before.
+```
+(function have5Pokemons() {
+    stubAmout(5)
+    howsMyCollection()
+})()
+```
+So, we changed the results to what we needed when we needed.
+
+The originalAmount method is also available, if we would need it at any point. In this particular case we also save time, by creating a stub, instead of mocking the entire class.
+
+For example we didn't change anything to say method. And now we can leverage it, and see that our test works as intended.
+
+Here is stub example `stubs.js`.
+
+How to debug JS:
+- https://code.visualstudio.com/docs/nodejs/nodejs-debugging
+- https://medium.com/the-node-js-collection/debugging-node-js-with-google-chrome-4965b5f910f4
---
+++ stubs.js
@@ -0,0 +1,50 @@
+// Custom Pokemons Implementation
+let Pokemons = {
+    // Method we want to replace
+    amount() {
+        // Call to database
+    },
+
+    // Method we eant to stay intact
+    say(message) {
+        console.log('Your Pokedex says: ', message)
+    }
+}
+
+//Function using Pokemons class
+function howsMyCollection() {
+    const size = Pokemons.amount()
+    if (size === undefined)
+        return Pokemons.say('Ooops, not sure how many you have')
+    if (size < 10)
+        return Pokemons.say('You only have few, you need more')
+    if (size < 50)
+        return Pokemons.say('You have quite some favorites. Keep going')
+
+    return Pokemons.say('You are quite a collector')
+}
+
+// Keep an implementation of original implementation for potential use later on
+const originalAmount = Pokemons.amount
+
+// Change the output of the amounts function
+function stubAmout(amount) {
+    Pokemons.amount = () => amount
+}
+
+// Testing function
+
+// (function have5Pokemons() {
+//     stubAmout(5)
+//     howsMyCollection()
+// })()
+
+function havePokemons(amount) {
+    stubAmout(amount)
+    howsMyCollection()
+}
+
+// A few tests to show outputs
+havePokemons(5)   // -- You only have a few, you need more
+havePokemons(17)  // -- You have quite some favorites. Keep going
+havePokemons(100) // -- You are quite a collector
